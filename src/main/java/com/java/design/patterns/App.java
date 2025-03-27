8f0575a50db91912522d9037a3d2810ae505a629
Mihail Cepraga <yahim09@gmail.com>
2025-03-27 10:06:02 +0000

    Add Apache Commons Lang dependency and implement serialization clone example

--- src/main/java/com/java/design/patterns/App.java
+++ src/main/java/com/java/design/patterns/App.java
@@ -1,24 +1,32 @@
 package com.java.design.patterns;
 
+import org.apache.commons.lang3.SerializationUtils;
+
 import com.java.design.patterns.creational.prototype.cloneable.Address;
 import com.java.design.patterns.creational.prototype.cloneable.Person;
 import com.java.design.patterns.creational.prototype.copyconstructors.Employee;
+import com.java.design.patterns.creational.prototype.serializable.Foo;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) throws Exception
-    {
-            runCloneableImplementation();
-            runCopyConstructorsImplementation();
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Implementing cloneable");
+        runCloneableImplementation();
+        System.out.println("=====================================");
+        System.out.println("Implementing copy constructors");
+        runCopyConstructorsImplementation();
+        System.out.println("=====================================");
+        System.out.println("Implementing clone with serializable");
+        runCloneWithSerializableImplementation();
     }
 
     public static void runCloneableImplementation() throws CloneNotSupportedException {
         Address address = new Address("123 London Road", 100);
-        Person john = new Person(new String[]{"John", "Smith"}, address);
+        Person john = new Person(new String[] { "John", "Smith" }, address);
 
         // Person jane = john; // Shallow copy
         Person jane = john.clone(); // Deep copy
@@ -31,7 +39,8 @@ public class App
     }
 
     public static void runCopyConstructorsImplementation() {
-        com.java.design.patterns.creational.prototype.copyconstructors.Address address = new com.java.design.patterns.creational.prototype.copyconstructors.Address("123 London Road", 100);
+        com.java.design.patterns.creational.prototype.copyconstructors.Address address = new com.java.design.patterns.creational.prototype.copyconstructors.Address(
+                "123 London Road", 100);
         Employee john = new Employee("John", address);
 
         Employee jane = new Employee(john);
@@ -40,5 +49,13 @@ public class App
 
         System.out.println(john);
         System.out.println(jane);
+    }
+
+    public static void runCloneWithSerializableImplementation() {
+        Foo foo = new Foo(42, "life");
+        Foo foo2 = SerializationUtils.roundtrip(foo);
+        foo2.whatever = "xyz";
+        System.out.println(foo);
+        System.out.println(foo2);
     }
 }
